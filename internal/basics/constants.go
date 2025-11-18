0de34a7fd3a63c5e93bc24978394f702bda3d390
xanity-07 <th3loot69@gmail.com>
2025-11-18 07:32:20 -0500

    constants and variable naming conventions

---
+++ internal/basics/constants.go
@@ -0,0 +1,10 @@
+package basics
+
+import "fmt"
+
+func Constants() {
+	const PI float64 = 3.14159
+	const GRAVITY float64 = 9.81
+
+	fmt.Println(PI, GRAVITY)
+}
---
+++ internal/basics/variables.go
@@ -0,0 +1,38 @@
+package basics
+
+// var middle string = "Jane"
+
+func Variables() {
+	//* Variable declarations
+	// var age int = 25
+	// var name = "Xanity"
+
+	//* Shorthand
+	// count := 10
+	// lastName := "Dev"
+
+	//? SCOPE
+	// fmt.Println(PrintFirstName(firstName))
+}
+
+//* SCOPE
+// func PrintFirstName() {
+// 	firstName:= "Xan"
+// 	fmt.Println(firstName)
+// }
+
+func NamingConventions() {
+	//* PascalCase
+	//? Structs. Interfaces, Enums
+	//? Eg. UserInfo, CalculateArea, NewHTTPRequest
+
+	//* snake_case
+	//? Eg, user_id, first_name, http_request
+
+	//* UPPERCASE
+	//? Consts - Recognizable
+	//? Immutable
+
+	//* camelCase
+	//? Eg. javaScript, htmlDocument, isValid
+}
