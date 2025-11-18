2d74edaf70d510408bbf371ff036a7a97a41a8a6
xanity-07 <th3loot69@gmail.com>
2025-11-18 12:48:20 -0500

    Operators

---
+++ internal/basics/opperators.go
@@ -0,0 +1,22 @@
+package basics
+
+//* Logical Operators
+//? ! (Logical Not)
+//? || (Logical OR)
+//? && (Logical AND)
+
+//*  Bitwise Operators
+//?  & (Bitwise AND)
+//?  | (Bitwise OR)
+//?  ^ (Bitwise XOR)
+//?  &^ (Bit clear, AND NOT)
+//?  << (Left Shift)
+//?  >> (Right Shift)
+
+//* Comparison Operators
+//?  == (Equal)
+//?  != (Not Equal)
+//?  < (Less Than)
+//?  <= (Less Than Or Equal)
+//?  > (Greater Than)
+//?  >= (Greater Than Or Equal)
--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
-import "github.com/xanity-07/golang/internal/basics"
-
 // "github.com/xanity-07/golang/internal/basics"
 
 func main() {
 	// basics.ImportsExample()
-	basics.Loops()
+	// basics.Loops()
 }
