42fe73efd1eeaa741b273d7b91ef216bb9ca7257
xanity-07 <th3loot69@gmail.com>
2025-12-03 12:01:55 -0500

    error handling

--- internal/intermediate/error_handling.go
+++ internal/intermediate/error_handling.go
@@ -1,5 +1,35 @@
 package intermediate
 
-func IntroErros() {
+import (
+	"errors"
+	"fmt"
+)
 
+func sqrt(x int) (int, error) {
+	if x < 0 {
+		return 0, errors.New("math error: square root of negative number")
+	}
+	return 1, nil
+}
+
+func process(data []byte) error {
+	if len(data) == 0 {
+		return errors.New("empty data set")
+	}
+	return nil
+}
+
+func IntroErros() {
+	res, err := sqrt(16)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(res)
+
+	data := []byte{}
+	if err := process(data); err != nil {
+		fmt.Println(err)
+		return
+	}
 }
